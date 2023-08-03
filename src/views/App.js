71934dbdfbe6cb530a3142de0a9ff26dc60bfe3a
annguyenqn <theesan2k1@gmail.com>
2023-08-03 15:49:41 +0700

    Router

--- src/views/App.js
+++ src/views/App.js
@@ -2,35 +2,51 @@ import logo from './logo.svg';
 import './App.scss';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Nav from './Nav/Nav';
 // import MyComponent from './Example/MyComponent';
 // 2 components : class component / funtion component( funtion , arrow)
 import MyComponent from './Example/MyComponent';
 import ListToDo from './ToDo/ListToDo'
 import Welcome from './Example/MyComponent'
+import Home from './Example/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ListToDo></ListToDo>
+    <BrowserRouter>
+      <div className="App">
+        <Nav></Nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Routes>
+            <Route exact path='/mycom' element={<MyComponent></MyComponent>} />
+            <Route exact path='/Todo' element={<ListToDo></ListToDo>} />
+            <Route exact path='/' element={<Home></Home>} />
+          </Routes>
+          {/* <MyComponent></MyComponent> */}
+        </header>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        {/* Same as */}
+        <ToastContainer />
 
-        {/* <MyComponent></MyComponent> */}
-      </header>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      {/* Same as */}
-      <ToastContainer />
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
